40db2030876d857c0ed595023f90d2d8d844e34d
Richard Lim <rclim95@outlook.com>
2020-06-12 22:39:26 -0500

    Add python-dotenv
    This package will allow us to read environment variables from a .env
    file.
    
    In addition, include a sample .env file containing the environment
    variables that playwhat.service is expecting (and print out the
    environment variable values in the logs, when the level is DEBUG).

--- playwhat/service/__main__.py
+++ playwhat/service/__main__.py
@@ -6,6 +6,7 @@ import os
 import signal
 import sys
 import daemon
+import dotenv
 from daemon.pidfile import PIDLockFile
 from playwhat.service import LOGGER, server, client
 from playwhat.service.constants import PATH_PID
@@ -36,6 +37,21 @@ def main():
 
     try:
         logger.info("Starting PlayingWHAT Service...")
+
+        # Determine if there's an environment variable called ENV_FILE. If so, we'll load the
+        # .env file specified in the ENV_FILE path
+        env_file_path = os.getenv("ENV_FILE", None)
+        if os.path.exists(env_file_path):
+            logger.info("Loading .env file from %s", env_file_path)
+            dotenv.load_dotenv(env_file_path)
+        else:
+            logger.info("Loading .env file from default location")
+            dotenv.load_dotenv()
+
+        # Show some useful information about the environment variables we've loaded
+        logger.debug("SPOTIFY_CLIENT_ID = %s", os.getenv("SPOTIFY_CLIENT_ID"))
+        logger.debug("SPOTIFY_CLIENT_SECRET = %s", "*" * len(os.getenv("SPOTIFY_CLIENT_SECRET")))
+
         with context:
             asyncio.run(server.start())
     except Exception:

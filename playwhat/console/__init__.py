94ea1711628c0096f35f5dfc89f46a8af5de5f28
Richard Lim <rclim95@outlook.com>
2020-06-13 22:47:09 -0500

    Introduce a main LOGGER for playwhat.console

---
+++ playwhat/console/__init__.py
@@ -0,0 +1,5 @@
+"""Provides a module for interacting with the `playwhat.service` daemon"""
+import logging
+
+# The main logger for the package.
+LOGGER = logging.getLogger(__package__)
--- playwhat/console/__main__.py
+++ playwhat/console/__main__.py
@@ -7,27 +7,26 @@ import sys
 import dotenv
 import spotipy
 import playwhat
+from playwhat.console import LOGGER
 
-def authenticate(args):
+def authenticate(dotenv_path: str, args):
     """Authenticate with the Spotify API"""
-    logger = logging.getLogger()
-    logger.info("Authenticating with the Spotify API...")
+    LOGGER.info("Authenticating with the Spotify API...")
 
     # We're going to be saving the user token into the dotenv file for future reference, so make
     # sure we can find it first before we go through the authorization process.
-    dotenv_path = args.dotenv
     if dotenv_path is None:
         try:
             dotenv_path = dotenv.find_dotenv(raise_error_if_not_found=True)
         except Exception:
-            logger.exception("Unable to find .ENV file. Please specify the path to the .ENV file "
-                             "using the --dotenv switch.")
+            LOGGER.exception("Unable to find .ENV file. Please specify the path to the .ENV file "
+                             "by setting an \"ENV_FILE\" environment variable")
             return
 
     # Prompt the user for their user token so we can observe what they're playing on Spotify
     #
     # TODO: Extract this to a constant.
-    logger.info("Prompting user for authentication...")
+    LOGGER.info("Prompting user for authentication...")
     scopes = ["user-read-recently-played", "user-read-playback-state"]
     cache_path = os.getenv(playwhat.ENV_CREDENTIAL_CACHE_PATH)
     token = spotipy.util.prompt_for_user_token(args.username,
@@ -40,12 +39,11 @@ def authenticate(args):
 
     # Save the user token for future reference
     dotenv.set_key(dotenv_path, playwhat.ENV_USER_TOKEN, token)
-    logger.info("User token saved to .env file, restart the service for your changes to take effect.")
+    LOGGER.info("User token saved to .env file, reload the service for your changes to take effect.")
 
 def create_argparser() -> ArgumentParser:
     """Creates the argument parser for this script"""
     args = ArgumentParser(description="Provides a way to interact with the playwhat.service daemon")
-    args.add_argument("--dotenv", metavar="file", help="The path to the .env file to load.")
     args.add_argument("-v", "--verbose", action="store_true", help="Print out debug information.")
 
     actions = args.add_subparsers(
@@ -68,21 +66,21 @@ def main():
     logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO,
                         format="[%(levelname)s] %(asctime)s - %(name)s: %(message)s",
                         stream=sys.stdout)
-    logger = logging.getLogger()
 
     # Print out the environment variables being used
-    if args.dotenv is None:
-        logger.info("Loading .env file from default location")
+    dotenv_path = os.getenv("ENV_FILE", None)
+    if dotenv_path is None:
+        LOGGER.info("Loading .env file from default location")
         dotenv.load_dotenv()
     else:
-        logger.info("Loading .env file from %s", args.dotenv)
-        dotenv.load_dotenv(args.dotenv)
+        LOGGER.info("Loading .env file from %s", dotenv_path)
+        dotenv.load_dotenv(dotenv_path)
 
-    logger.debug("SPOTIFY_CLIENT_ID = %s", os.getenv(playwhat.ENV_CLIENT_ID))
-    logger.debug("SPOTIFY_CLIENT_SECRET = %s", "*" * len(os.getenv(playwhat.ENV_CLIENT_SECRET)))
+    LOGGER.debug("SPOTIFY_CLIENT_ID = %s", os.getenv(playwhat.ENV_CLIENT_ID))
+    LOGGER.debug("SPOTIFY_CLIENT_SECRET = %s", "*" * len(os.getenv(playwhat.ENV_CLIENT_SECRET)))
 
     # Run the action the user as speciifed
-    args.func(args)
+    args.func(dotenv_path, args)
 
 if __name__ == "__main__":
     main()

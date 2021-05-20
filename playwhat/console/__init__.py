fe4d4022634dc697bcb0753e493162e7379cd224
Richard Lim <rclim95@outlook.com>
2021-05-20 16:54:56 -0500

    Begin the rewrite process of PlayingWHAT
    PlayingWHAT will be rewritten to be bigger and better than before! And
    by bigger and better, I'm hoping to make PlayingWHAT work _primarily_ on
    the Inky Impression screen (no guarantee it'll work on the Inky wHAT
    like before, but that may get reinstated in some future commits later,
    once the Inky Impression build is complete) and make it easier to manage
    (via the user's web browser instead of having to the use the
    playwhat.console application). :smile:

--- playwhat/console/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Provides a module for interacting with the `playwhat.service` daemon"""
-import logging
-
-# The main logger for the package.
-LOGGER = logging.getLogger(__package__)
--- playwhat/painter/screens/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Initializes the `playwhat.painter.screens` module"""
-import playwhat.painter.screens.not_playing
-import playwhat.painter.screens.now_playing
-import playwhat.painter.screens.recently_played
